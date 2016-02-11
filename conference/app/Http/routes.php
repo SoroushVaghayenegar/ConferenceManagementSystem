c10491810e4e7c0a37e614f1dd4a41725e8d0fcf
Yuwei Wang <3772014@gmail.com>
2016-02-11 14:29:48 -0800

    new version with vanilla ui

--- conference/app/Http/routes.php
+++ conference/app/Http/routes.php
@@ -35,3 +35,50 @@ Route::group(['middleware' => 'web'], function () {
 
     Route::get('/home', 'HomeController@index');
 });
+
+use App\Conference;
+use Illuminate\Http\Request;
+
+Route::group(['middleware' => 'web'], function () {
+
+    /**
+     * Show Conference Dashboard
+     */
+    Route::get('/', function () {
+    	$conferences = Conference::orderBy('created_at', 'asc')->get();
+
+    	return view('conferences', [
+        'conferences' => $conferences
+    ]);
+        //
+    });
+
+    /**
+     * Add New Conference
+     */
+    Route::post('/conference', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    $conference = new Conference;
+    $conference->name = $request->name;
+    $conference->save();
+
+    return redirect('/');
+    // Create conference
+});
+
+    Route::delete('/conference/{conference}', function (Conference $conference) {
+    $conference->delete();
+
+    return redirect('/');
+});
+
+});
---
+++ conference/resources/views/conferences.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="col-sm-offset-2 col-sm-8">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    New Conference
+                </div>
+
+                <div class="panel-body">
+                    <!-- Display Validation Errors -->
+                    @include('common.errors')
+
+                    <!-- New Conference Form -->
+                    <form action="/conference" method="POST" class="form-horizontal">
+                        {{ csrf_field() }}
+
+                        <!-- Conference Name -->
+                        <div class="form-group">
+                            <label for="conference-name" style="width:100%" class="control-label" Conference</label>
+
+                            <div class="col-sm-6">
+                                <input type="text" name="name" id="conference-name" class="form-control" value="{{ old('conference') }}">
+                            </div>
+                        </div>
+
+                        <!-- Add Conference Button -->
+                        <div class="form-group">
+                            <div class="col-sm-offset-3 col-sm-6">
+                                <button type="submit" class="btn btn-default">
+                                    <i class="fa fa-plus"></i>Add Conference
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <!-- Current Conferences -->
+            @if (count($conferences) > 0)
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        Current Conferences
+                    </div>
+
+                    <div class="panel-body">
+                        <table class="table table-striped conference-table">
+                            <thead>
+                                <th Conference</th>
+                                <th>&nbsp;</th>
+                            </thead>
+                            <tbody>
+                                @foreach ($conferences as $conference)
+                                    <tr>
+    <!-- Name -->
+    <td class="table-text">
+        <div>{{ $conference->name }}</div>
+    </td>
+
+    <!-- Delete Button -->
+    <td>
+        <form action="{{ url('conference/'.$conference->id) }}" method="POST">
+            {!! csrf_field() !!}
+            {!! method_field('DELETE') !!}
+
+            <button type="submit" class="btn btn-danger">
+                <i class="fa fa-trash"></i> Delete
+            </button>
+        </form>
+    </td>
+</tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            @endif
+        </div>
+    </div>
+@endsection
