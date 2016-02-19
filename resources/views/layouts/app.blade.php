b26201c0a4a8d77bb687950145305ae97f09391e
soroush <sorooshvgr@gmail.com>
2016-02-18 21:42:28 -0800

    /welcome is the homepage of the website made some small changes in the homepage

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Conference</title>
+    <title>@yield('title')</title>
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700" rel="stylesheet" type="text/css">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css">
@@ -14,10 +14,15 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0-alpha1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 
+    
+
+    
+
     <style>
         body {
             font-family: 'Raleway';
             margin-top: 25px;
+            background-image: url('dark_embroidery.png');
         }
 
         button .fa {
@@ -40,20 +45,24 @@
     <div class="container">
         <nav class="navbar navbar-default">
             <div class="container-fluid">
-                <div class="navbar-header">
+                <div class="navbar-header" align="left">
                     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
                         <span class="sr-only">Toggle navigation</span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                         <span class="icon-bar"></span>
                     </button>
-
-                    <a class="navbar-brand" href="#">Conference List</a>
+                    <a class="navbar-brand" >Gobind Sarvar</a>
+                    
                 </div>
 
                 <div id="navbar" class="navbar-collapse collapse">
                     <ul class="nav navbar-nav">
-                        &nbsp;
+                    </ul>
+
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">About Us</a></li>
+                        <li><a href="{{ URL::to('login') }}" class="btn btn-primary" style="color:white;">Login!</a></li>
                     </ul>
                 </div>
             </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,14 +1,19 @@
 @extends('layouts.app')
 
+@section('title', 'Gobind Sarvar Conference')
+
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
-            <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+            <div class="panel panel-default" style='opacity:0.8'>
+                <div class="panel-heading">
+                    <h1 style="text-align: center;">Welcome to Gobind Sarvar Conferences!</h1>
+                </div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <h2 style="text-align: center;">Don't have an account?</h2>
+                    <a href="{{ URL::to('register') }}" class="btn btn-success" style="position:relative;left: 40%;font-size: 200%;">Sign Up Here!</a>
                 </div>
             </div>
         </div>
