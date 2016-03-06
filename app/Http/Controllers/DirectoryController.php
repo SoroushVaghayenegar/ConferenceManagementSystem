93f577392c2057e835dfdf79b29bb2b8a586e3b9
soroush <sorooshvgr@gmail.com>
2016-03-06 15:37:32 -0800

    fixed past conferences bug

--- app/Http/Controllers/DirectoryController.php
+++ app/Http/Controllers/DirectoryController.php
@@ -20,9 +20,9 @@ class DirectoryController extends Controller
 
     public function index()
     {
-        $current_conferences = DB::table('conferences')->where('end','>=', time())->get();
+        $current_conferences = DB::table('conferences')->where('end', '>=', date('Y-m-d').' 00:00:00')->get();
 
-        $past_conferences = DB::table('conferences')->where('end','=<', time())->get();
+        $past_conferences = DB::table('conferences')->where('end', '<=', date('Y-m-d').' 00:00:00')->get();
 
         return view('directory', ['current_conferences' => $current_conferences , 'past_conferences' => $past_conferences]);
         
