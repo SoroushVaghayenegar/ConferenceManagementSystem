f1bc78d69014e68580148aca1c66053d3a74a3f2
Jonathan Lui <kinwa91@msn.com>
2016-03-28 23:18:13 -0700

    fix styles in manage participants

--- resources/views/manage_participants.blade.php
+++ resources/views/manage_participants.blade.php
@@ -11,53 +11,49 @@
 </div>
 
 <div class="container">
-  <div class="panel panel-dark" >
-    <div class="tab-content">
-      <div id="currentConferences" class="tab-pane fade in active">
-        <div class="panel-body">
-          <select id="current_conferences" class="form-control" style="background-color: #006bb3; color:#d9d9d9">
-            <option value="#">Select a conference</option>
-            @if (count($conferences) > 0)
-            @foreach ($conferences as $conference)
-            <option value="/conference/{{$conference->id}}/participants">{{$conference->name}}</option>
-            @endforeach
-            @else
-            <option>No conferences available!</option>
-            @endif
-          </select>
-          <h3 style="color:white; text-align: center"><strong>Participants</strong></h3>
-          <table id="participants_table_current" class="table table-striped table-bordered" cellspacing="0" width="100%">
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Phone No</th>
-                <th>Flight No</th>
-                <th>Arrival date</th>
-                <th>Arrival time</th>
-                <th>Hotel requested</th>
-                <th>Taxi requested</th>
-              </tr>
-            </thead>
-            <tbody>
-              @if (isset($attendees))
-              @foreach ($attendees as $attendee)
-              <tr>
-                <td>{{$attendee->name}}</td>
-                <td>{{$attendee->age}}</td>
-                <td>{{$attendee->phone}}</td>
-                <td>{{$attendee->flight ? $attendee->flight : "N/A"}}</td>
-                <td>{{$attendee->arrival_date ? $attendee->arrival_date : "N/A"}}</td>
-                <td>{{$attendee->arrival_time ? $attendee->arrival_time : "N/A"}}</td>
-                <td>{{$attendee->hotel_requested ? "Yes": "No"}}</td>
-                <td>{{$attendee->taxi_requested ? "Yes": "No"}}</td>
-              </tr>
-              @endforeach
-              @endif
-            </tbody>
-          </table>
-        </div>
-      </div>
+  <div class="panel panel-default">
+    <div class="panel-body">
+      <select id="current_conferences" class="form-control">
+        <option value="#">Select a conference</option>
+        @if (count($conferences) > 0)
+        @foreach ($conferences as $conference)
+        <option value="/conference/{{$conference->id}}/participants">{{$conference->name}}</option>
+        @endforeach
+        @else
+        <option>No conferences available!</option>
+        @endif
+      </select>
+      <h3 style="color:white; text-align: center"><strong>Participants</strong></h3>
+      <table id="participants_table_current" class="table table-striped table-bordered" cellspacing="0" width="100%">
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Age</th>
+            <th>Phone No</th>
+            <th>Flight No</th>
+            <th>Arrival date</th>
+            <th>Arrival time</th>
+            <th>Hotel requested</th>
+            <th>Taxi requested</th>
+          </tr>
+        </thead>
+        <tbody>
+          @if (isset($attendees))
+          @foreach ($attendees as $attendee)
+          <tr>
+            <td>{{$attendee->name}}</td>
+            <td>{{$attendee->age}}</td>
+            <td>{{$attendee->phone}}</td>
+            <td>{{$attendee->flight ? $attendee->flight : "N/A"}}</td>
+            <td>{{$attendee->arrival_date ? $attendee->arrival_date : "N/A"}}</td>
+            <td>{{$attendee->arrival_time ? $attendee->arrival_time : "N/A"}}</td>
+            <td>{{$attendee->hotel_requested ? "Yes": "No"}}</td>
+            <td>{{$attendee->taxi_requested ? "Yes": "No"}}</td>
+          </tr>
+          @endforeach
+          @endif
+        </tbody>
+      </table>
     </div>
   </div>
 </div>
