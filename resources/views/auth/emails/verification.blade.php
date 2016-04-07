a1f2445d71731b65a074807c68431654ce2c86ca
Sharareh <sparkshfx@yahoo.com>
2016-04-06 18:48:52 -0700

    Fixed grammar error sarver to "Sarvar" in email verification

--- resources/views/auth/emails/verification.blade.php
+++ resources/views/auth/emails/verification.blade.php
@@ -8,10 +8,10 @@
         <meta charset="utf-8">
     </head>
     <body>
-        <h2>Welcome to Gobind Sarver!</h2>
+        <h2>Welcome to Gobind Sarvar!</h2>
 
         <div>
-            Thank you for signing up with Gobind Sarver. Please verify your email address by clicking the link below so that we can send you upcoming information. <br><br>
+            Thank you for signing up with Gobind Sarvar. Please verify your email address by clicking the link below so that we can send you upcoming information. <br><br>
              {{ URL::to('/verify/'.$verification_code ) }}
 
         </div>
